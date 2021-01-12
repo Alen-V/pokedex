35384a4209764a45974f5519ba2b48b0a062b475
Filip <alempijevik@gmail.com>
2021-01-12 16:10:56 +0100

    post functionality

--- assets/js/style.js
+++ assets/js/style.js
@@ -1,1 +1,63 @@
-console.log('works')
+let test = document.getElementsByClassName('pokemon-list')[0];
+let data;
+
+async function fetchData(){
+    let call = await fetch("../../pokedex.json");
+    let data = await call.json();
+    console.log(data);
+    return data;
+}
+
+function createPokemon (id, name, sprite) {
+    let result;
+    let start = `<div class="pokemon-list-item item-active">`;
+    let end = `</div>`;
+    result = `
+        ${start}
+        <img src="${sprite}" class="pokemon-list-image">
+        <div class="pokemon-id"><h2>${createId(id)}</h2></div>
+        <div class="pokemon-name"><h2>${name}</h2></div>
+        <div class="pokeball-list-icon"><img src="assets/images/pokeball-list-icon.svg" alt=""></div>
+    `
+    result += `${end}`;
+    test.innerHTML += result;
+}
+
+function postPokemon () {
+    data = fetchData()
+    .then(data => {
+        for (let i = 0; i < data.length; i++) {
+            createPokemon(data[i].id, data[i].name, matchSprite(data[i].id));
+        }
+    })
+}
+
+function matchSprite (id) {
+    let result = id.split('');
+    switch (result.length) {
+        case 2:
+            result = `./assets/images/sprites/0${id}MS.PNG`;
+            return result;
+        case 3:
+            result = `./assets/images/sprites/${id}MS.PNG`;
+            return result;
+    }
+}
+
+function createId (id) {
+    let result = id.split('');
+    switch (result.length) {
+        case 2:
+            result = `No. 0${id}`;
+            return result;
+        case 3:
+            result = `No. ${id}`;
+            return result;
+    }
+}
+
+function insertSprite () {
+    let sprite = document.getElementsByClassName('pokemon-list-image')[0]
+}
+
+postPokemon();
